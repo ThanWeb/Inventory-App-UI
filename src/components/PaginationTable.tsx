14324033f85783e2eb87dd15124f351c6027c24e
Hans <classsickpeople@gmail.com>
2024-01-18 16:26:47 +0800

    Finish add, edit, show detail and delete product in homepage and use ProductInputModal

--- src/components/PaginationTable.tsx
+++ src/components/PaginationTable.tsx
@@ -1,13 +1,11 @@
 import { useState, useEffect } from 'react'
 import type { ReactElement, ChangeEvent } from 'react'
+import translateProductProps from '@/utils/translateProductProps'
 import type IProduct from '@/types/product'
 
 interface IPaginationTable {
   searchQuery: string
-  toggleModalHandler: (status: string, items: Record<string, any>) => void
-  toggleDeleteModalHandler: (value: Record<string, any>) => void
-  setDetailModalData: (value?: Record<string, any>) => void
-  setIsDetailModalActive: (value?: boolean) => void
+  showProductModalForAction: (action: string, product: IProduct | null, id: number) => void
 }
 
 interface IProps {
@@ -18,7 +16,7 @@ interface IProps {
 const PaginationTable = ({ rawItems, props }: IProps): ReactElement => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [itemsShowedOption, setItemsShowedOption] = useState([5, 15, 25, 50, 100])
-  const [splitedItems, setSplitedItems] = useState<Array<Array<Record<string, any>>>>([[]])
+  const [splitedItems, setSplitedItems] = useState<IProduct[][]>([[]])
   const [paginationItem, setPaginationItem] = useState(itemsShowedOption[0])
   const [activePagination, setActivePagination] = useState(0)
   const [propsItem, setPropsItem] = useState<string[]>([])
@@ -30,10 +28,6 @@ const PaginationTable = ({ rawItems, props }: IProps): ReactElement => {
   useEffect(() => {
     paginationItemChangeHandler()
   }, [paginationItem])
-
-  useEffect(() => {
-    console.log(propsItem)
-  }, [propsItem])
 
   const splitItemsIntoPaginationAndGetItemNames = (): void => {
     if (rawItems.length > 0) {
@@ -68,11 +62,6 @@ const PaginationTable = ({ rawItems, props }: IProps): ReactElement => {
     setPaginationItem(parseInt(event.target.value))
   }
 
-  // const showDetailModal = (data: Record<string, any>): void => {
-  //   props.setIsDetailModalActive(true)
-  //   props.setDetailModalData(data)
-  // }
-
   return (
     <div>
       { rawItems.length <= 0
@@ -92,31 +81,52 @@ const PaginationTable = ({ rawItems, props }: IProps): ReactElement => {
           </div>
           <div>
             {
-              splitedItems.map((items: Array<Record<string, any>>, parentIndex: number) =>
+              splitedItems.map((items: IProduct[], parentIndex: number) =>
                 <table key={parentIndex} className={`${parentIndex !== activePagination ? 'hidden' : 'block'} text-center`}>
                   <tbody>
                     <tr>
                       <th className='px-3 border'>No</th>
-                      {propsItem.map((header, index) => <th key={index} className='px-3 border'>{header}</th>)}
+                      {propsItem.map((header, index) => <th key={index} className='px-3 border'>{translateProductProps(header)}</th>)}
                       <th className='px-3 border'>Aksi</th>
                     </tr>
                     {
-                      items.map((item: Record<string, any>, childIndex: number) =>
+                      items.map((item: IProduct, childIndex: number) =>
                         <tr key={childIndex}>
                           <td className='px-3 border'>
                             <span>{(paginationItem * parentIndex) + (childIndex + 1)}</span>
                           </td>
                           {
-                            propsItem.map((prop, index) =>
-                              <td key={index} className='px-3 border'>
-                                <span>{item[prop]}</span>
+                            propsItem.map((prop, index) => {
+                              if (prop === 'createdAt' || prop === 'updatedAt') {
+                                return (
+                                  <td key={index} className='px-3 border'>
+                                    <span>{new Date(`${item[prop]}`).toLocaleString()}</span>
+                                  </td>
+                                )
+                              } else if (
+                                prop === 'name' ||
+                                prop === 'capitalPrice' ||
+                                prop === 'sellPrice' ||
+                                prop === 'stock' ||
+                                prop === 'unit'
+                              ) {
+                                return <td key={index} className='px-3 border'>
+                                  <span>{item[prop]}</span>
+                                </td>
+                              }
+
+                              return <td key={index} className='px-3 border'>
+                                <span></span>
                               </td>
+                            }
+
                             )
                           }
                           <td className='px-3 border'>
-                            <div>
-                              <button onClick={() => { props.toggleModalHandler('edit', item) }}>Edit</button>
-                              <button onClick={() => { props.toggleDeleteModalHandler(item) }}>Delete</button>
+                            <div className='flex gap-x-2'>
+                              <button type='button' onClick={() => { props.showProductModalForAction('detail', item, 0) }}>Detail</button>
+                              <button type='button' onClick={() => { props.showProductModalForAction('edit', item, item.id ?? 0) }}>Ubah</button>
+                              <button type='button' onClick={() => { props.showProductModalForAction('delete', item, item.id ?? 0) }}>Hapus</button>
                             </div>
                           </td>
                         </tr>
@@ -124,7 +134,7 @@ const PaginationTable = ({ rawItems, props }: IProps): ReactElement => {
                     }
                     <tr>
                       <th className='px-3 border'>No</th>
-                      {propsItem.map((header, index) => <th key={index} className='px-3 border'>{header}</th>)}
+                      {propsItem.map((header, index) => <th key={index} className='px-3 border'>{translateProductProps(header)}</th>)}
                       <th className='px-3 border'>Aksi</th>
                     </tr>
                   </tbody>
@@ -137,11 +147,11 @@ const PaginationTable = ({ rawItems, props }: IProps): ReactElement => {
             <span>Menampilkan {activePagination + 1} dari {splitedItems.length} halaman</span>
           </div>
           <div>
-            {activePagination - 2 >= 0 && <button onClick={() => { changePagination(2 * -1) }}>Left</button>}
-            {activePagination !== 0 && <button onClick={() => { changePagination(-1) }}>{activePagination}</button>}
-            <button>{activePagination + 1}</button>
-            {activePagination !== splitedItems.length - 1 && <button onClick={() => { changePagination(1) }}>{activePagination + 2}</button>}
-            {activePagination + 2 < splitedItems.length && <button onClick={() => { changePagination(2) }}>Right</button>}
+            {activePagination - 2 >= 0 && <button type='button' onClick={() => { changePagination(2 * -1) }}>Left</button>}
+            {activePagination !== 0 && <button type='button' onClick={() => { changePagination(-1) }}>{activePagination}</button>}
+            <button type='button'>{activePagination + 1}</button>
+            {activePagination !== splitedItems.length - 1 && <button type='button' onClick={() => { changePagination(1) }}>{activePagination + 2}</button>}
+            {activePagination + 2 < splitedItems.length && <button type='button' onClick={() => { changePagination(2) }}>Right</button>}
           </div>
         </div>
       }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { asyncGetProducts, asyncAddProduct, asyncEditProduct, asyncDeleteProduct } from '@/store/products/action'
 import type { RootState } from '@/store/index'
 import PaginationTable from '@/components/PaginationTable'
+import ProductInputModal from '@/components/ProductInputModal'
 import type IProduct from '@/types/product'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -14,26 +15,121 @@ export default function Home (): ReactElement {
   const products: IProduct[] | never[] = useSelector((state: RootState) => state.products)
 
   const [searchQuery, setSearchQuery] = useState('')
+  const [isProductModalShowed, setIsProductModalShowed] = useState(false)
+  const [selectedAction, setSelectedAction] = useState('')
+
+  const [id, setId] = useState(0)
+  const [name, setName] = useState('')
+  const [capitalPrice, setCapitalPrice] = useState(0)
+  const [sellPrice, setSellPrice] = useState(0)
+  const [stock, setStock] = useState(0)
+  const [unit, setUnit] = useState('')
 
   useEffect(() => {
     dispatch(asyncGetProducts())
   }, [dispatch])
 
+  const showProductModalForAction = (action: string, product: IProduct | null, selectedId: number): void => {
+    setIsProductModalShowed(true)
+    setSelectedAction(action)
+
+    if (action === 'add') {
+      setName('')
+      setCapitalPrice(0)
+      setSellPrice(0)
+      setStock(0)
+      setUnit('')
+    } else {
+      if (product !== null) {
+        setId(selectedId)
+        setName(product.name)
+        setCapitalPrice(product.capitalPrice)
+        setSellPrice(product.sellPrice)
+        setStock(product.stock)
+        setUnit(product.unit)
+      }
+    }
+  }
+
+  const productModalSubmitHandler = async (event: React.FormEvent<HTMLFormElement>, action: string): Promise<void> => {
+    event.preventDefault()
+
+    try {
+      switch (action) {
+      case 'add':
+        await dispatch(asyncAddProduct({ name, capitalPrice, sellPrice, stock, unit }))
+        break
+      case 'edit':
+        await dispatch(asyncEditProduct({ id, product: { name, capitalPrice, sellPrice, stock, unit } }))
+        break
+      case 'delete':
+        await dispatch(asyncDeleteProduct({ id }))
+        break
+      default:
+        break
+      }
+
+      setIsProductModalShowed(false)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const checkIsThereAnyChange = (): boolean => {
+    const product = products.filter((item) => item.id === id)
+
+    if (product.length > 0) {
+      if (
+        product[0].name !== name ||
+        product[0].capitalPrice !== capitalPrice ||
+        product[0].sellPrice !== sellPrice ||
+        product[0].stock !== stock ||
+        product[0].unit !== unit
+      ) {
+        return true
+      } else {
+        return false
+      }
+    }
+
+    return false
+  }
+
   return (
     <main className={`flex flex-col ${inter.className}`}>
-      <div>
-        <input type='text' placeholder='Cari item' value={searchQuery} onChange={(event) => { setSearchQuery(event.target.value) }}/>
+      <div className='container mx-auto flex flex-col items-center'>
+        <div>
+          <button type='button' onClick={() => { showProductModalForAction('add', null, 0) }} >Tambah</button>
+          <input type='text' placeholder='Cari item' value={searchQuery} onChange={(event) => { setSearchQuery(event.target.value) }}/>
+        </div>
+        <PaginationTable
+          rawItems={products.filter((product) => { return product.name.toLowerCase().includes(searchQuery.toLowerCase()) })}
+          props={{
+            searchQuery,
+            showProductModalForAction
+          }}
+        />
       </div>
-      <PaginationTable
-        rawItems={products.filter((product) => { return product.name.toLowerCase().includes(searchQuery.toLowerCase()) })}
-        props={{
-          searchQuery,
-          toggleModalHandler: (status: string, items: Record<string, any>) => {},
-          toggleDeleteModalHandler: (value: Record<string, any>) => {},
-          setDetailModalData: (value?: Record<string, any>) => {},
-          setIsDetailModalActive: (value?: boolean) => {}
-        }}
-      />
+      <div className={`${isProductModalShowed ? 'fixed z-50' : 'hidden'} w-screen h-screen`}>
+        <ProductInputModal
+          props={{
+            name,
+            setName,
+            capitalPrice,
+            setCapitalPrice,
+            sellPrice,
+            setSellPrice,
+            stock,
+            setStock,
+            unit,
+            setUnit,
+            selectedAction,
+            isThereAnyChange: checkIsThereAnyChange(),
+            setIsProductModalShowed,
+            productModalSubmitHandler
+          }}
+        />
+      </div>
     </main>
   )
 }
