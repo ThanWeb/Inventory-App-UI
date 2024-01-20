14034f535d854b712984ac6ba4f00bbbfef1e593
Hans <classsickpeople@gmail.com>
2024-01-20 17:26:01 +0800

    Delete message after not null message

--- src/layouts/auth.tsx
+++ src/layouts/auth.tsx
@@ -1,20 +1,18 @@
 import { useEffect, type ReactElement } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
-import { useDispatch, useSelector } from 'react-redux'
 import { type RootState } from '@/store'
 import { unsetMessageActionCreator, type IStateMessage } from '@/store/message/action'
-import { type IStateUser } from '@/store/user/action'
 
-interface IDefaultLayoutProps {
+interface IAuthPageLayoutProps {
   children: React.ReactNode
 }
 
 const inter = Inter({ subsets: ['latin'] })
 
-const DefaultLayout = ({ children }: IDefaultLayoutProps): ReactElement => {
+const AuthPageLayout = ({ children }: IAuthPageLayoutProps): ReactElement => {
   const dispatch = useDispatch()
-  const user: IStateUser | null = useSelector((state: RootState) => state.user)
   const message: IStateMessage | null = useSelector((state: RootState) => state.message)
   const isLoading: boolean = useSelector((state: RootState) => state.isLoading)
 
@@ -33,9 +31,6 @@ const DefaultLayout = ({ children }: IDefaultLayoutProps): ReactElement => {
         <link rel='icon' href='/favicon/favicon.ico' sizes='any' />
         <link rel='apple-touch-icon' href='/favicon/apple-touch-icon.png' type='image/png' sizes='any' />
       </Head>
-      <header className={inter.className}>
-        <p>{user !== null ? user.username : 'Guest'}</p>
-      </header>
       <main className={inter.className}>
         {children}
       </main>
@@ -43,11 +38,8 @@ const DefaultLayout = ({ children }: IDefaultLayoutProps): ReactElement => {
         <p>{message?.text}</p>
         <p>{isLoading ? 'Sabar Lagi Loading' : 'Udah Gak Loading'}</p>
       </div>
-      <footer className={inter.className}>
-        Ini Footer
-      </footer>
     </>
   )
 }
 
-export default DefaultLayout
+export default AuthPageLayout
