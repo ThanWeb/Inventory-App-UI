a1f096d5bd78c40861f37621d1175746233e7fb5
Hans <classsickpeople@gmail.com>
2024-01-22 17:24:43 +0800

    Create not found page

---
+++ src/pages/404.tsx
@@ -0,0 +1,23 @@
+import type { ReactElement } from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+
+const NotFound = (): ReactElement => {
+  return (
+    <>
+      <Head>
+        <title>Not Found</title>
+        <meta name='description' content='Too bad, 404 error occured' />
+      </Head>
+      <main>
+        <div className='h-screen flex justify-center items-center gap-x-3'>
+          <h1 className='font-bold text-2xl'>404</h1>
+          <span className='text-3xl'>|</span>
+          <p className='text-xl font-medium'>Not Found, Back <Link href={'/'} className='text-amber-700 font-bold'>Home</Link></p>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default NotFound
